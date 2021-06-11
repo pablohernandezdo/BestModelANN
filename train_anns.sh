86a83dc7f6d48d96ce0cdc9f9ad92be62699876b
Pablo Hernandez <pablo.hernandezdo.13@sansano.usm.cl>
2021-06-11 01:38:46 -0400

    Train 3 models in parallel

--- train_anns.sh
+++ train_anns.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
 
-echo "Training model Ann_6k_6k, lr = 1e-3, epochs = 5, batch_size = 128"
+echo "Training model Ann_6k_6k, lr = 1e-3, epochs = 5, batch_size = 256"
 python train.py \
         --lr 1e-3 \
         --epochs 5 \
-        --batch_size 128 \
+        --batch_size 256 \
         --earlystop 0 \
         --eval_iter 1 \
         --model_folder 'models'  \
         --classifier ANN_6k_6k \
-        --model_name ANN_6k_6k_1e3_128 \
-        --dataset_name "STEAD" \
-        --train_path "Data/TrainReady/STEAD-STEAD_0.8_train.npy" \
-        --val_path "Data/TrainReady/STEAD-STEAD_0.1_val.npy"
+        --model_name ANN_6k_6k_1e3_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P1=$!
+
+echo "Training model Ann_6k_6k, lr = 1e-4, epochs = 5, batch_size = 256"
+python train.py \
+        --lr 1e-4 \
+        --epochs 5 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier ANN_6k_6k \
+        --model_name ANN_6k_6k_1e4_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P2=$!
+
+echo "Training model Ann_6k_6k, lr = 1e-5, epochs = 5, batch_size = 256"
+python train.py \
+        --lr 1e-5 \
+        --epochs 5 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier ANN_6k_6k \
+        --model_name ANN_6k_6k_1e5_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy"
+
+P3=$!
+wait $P1 $P2 $P3
