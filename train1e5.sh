db7ecdf1c8e7e16f3316b4e6260fb631a00b43e0
Pablo Hernandez <pablo.hernandezdo.13@sansano.usm.cl>
2021-07-27 01:06:47 -0400

    train lower learning rate model

---
+++ train1e5.sh
@@ -0,0 +1,15 @@
+# ANN_6K_6K
+echo "Training model Ann_3k_1k, lr = 1e-5, epochs = 30, batch_size = 256"
+python train.py \
+        --lr 1e-5 \
+        --device 3 \
+        --epochs 30 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 30 \
+        --model_folder 'models'  \
+        --classifier ANN_3k_1k \
+        --model_name ANN_1k_1k_30epch \
+        --dataset_name "STEAD" \
+        --train_path "Data/TrainReady/Extracto-STEAD_0.8_train.npy" \
+        --val_path "Data/TrainReady/Extracto-STEAD_0.1_val.npy"
